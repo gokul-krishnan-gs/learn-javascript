0804b07a55393f9df56f3d16dae54e608939a2ac
Gokul Krishnan <gokulkrishnangs02@gmail.com>
2025-08-19 11:35:47 +0530

    Create conditionalStatements.js

---
+++ control-flow/conditionalStatements.js
@@ -0,0 +1,17 @@
+//conditional statements
+
+let password;
+password = 8;
+console.log(password);
+if(password === 8){
+  console.log("Welcome");
+}
+else if(password <= 8){
+  console.log("Password is to short");
+}
+else if (password >= 8){
+  console.log("Too Long Password & Password should be 8 characters");
+}
+else{
+  console.log("Please provide a password");
+}
