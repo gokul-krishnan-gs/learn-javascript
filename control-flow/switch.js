f85d681fe272de85350b97c292b29d101ec5c739
Gokul Krishnan <gokulkrishnangs02@gmail.com>
2025-08-19 11:37:05 +0530

    Create switch.js

---
+++ control-flow/switch.js
@@ -0,0 +1,17 @@
+//switch conditional statements
+
+let fruit = 'apple';
+
+switch(fruit){
+  case 'banana':
+    console.log('Banana is good');
+    break;
+  case 'orange':
+    console.log('I am not a fan of orange');
+    break;
+  case 'apple':
+    console.log('How you like them apples?');
+    break;
+  default:
+    console.log('I have never heard of that fruit');
+}
